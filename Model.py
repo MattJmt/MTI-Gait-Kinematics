4f96ad0bd8386f0614f1a0a32dd56246e7530f1b
MattJmt <matjammot@gmail.com>
2023-11-22 17:36:06 +0100

    Serial to Python + Animation

---
+++ Model.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+from Serial import t, px1, px2, px3, py1, py2, py3, pz1, pz2, pz3
+
+# Function to generate joint positions using externally provided lists
+def generate_joint_positions(x_lists, y_lists, z_lists, joint_index, frame):
+    x_positions = x_lists[joint_index]
+    y_positions = y_lists[joint_index]
+    z_positions = z_lists[joint_index]
+    
+    x = x_positions[frame]
+    y = y_positions[frame]
+    z = z_positions[frame]
+    
+    return x, y, z
+
+time_list = t
+x_lists = [px1, px2, px3]
+y_lists = [py1, py2, py3]
+z_lists = [pz1, pz2, pz3]
+
+# Number of joints
+num_joints = len(x_lists)
+
+# Create a figure and axis for the 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim([-1.5, 1.5])  # Adjust the x-axis limits as needed
+ax.set_ylim([-1.5, 1.5])  # Adjust the y-axis limits as needed
+ax.set_zlim([0, 1.2])  # Adjust the z-axis limits as needed
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+ax.set_title('3D Animation of Joints')
+
+# # Initialize empty lines for the joints
+# lines = [ax.plot([], [], [], marker='o', markersize=5)[0] for _ in range(num_joints)]
+# Initialize empty lines for the joints
+lines = [ax.plot([], [], [], marker='o')[0] for _ in range(num_joints-1)]
+
+time_text = ax.text2D(0.05, 0.95, '', transform=ax.transAxes)
+
+# Function to update the plot for each frame of the animation
+def update(frame):
+    for i in range(num_joints):
+        x, y, z = generate_joint_positions(x_lists, y_lists, z_lists, i, frame)
+        # lines[i].set_data([x], [y])
+        # lines[i].set_3d_properties([z])
+        if i > 0:
+            # Update lines to connect the joints
+            lines[i - 1].set_data([x_lists[i - 1][frame], x_lists[i][frame]], [y_lists[i - 1][frame], y_lists[i][frame]])
+            lines[i - 1].set_3d_properties([z_lists[i - 1][frame], z_lists[i][frame]])
+    time_text.set_text('Time: {:.2f} seconds'.format(time_list[frame]))
+    return lines + [time_text]
+
+# Function to update the view angles for rotation (azimuth and elevation)
+def update_view(event):
+    ax.view_init(elev=elevation_slider.val, azim=azimuth_slider.val)
+
+# Create sliders for interactive control of elevation and azimuth
+elevation_slider_ax = plt.axes([0.1, 0.02, 0.65, 0.03])
+azimuth_slider_ax = plt.axes([0.1, 0.06, 0.65, 0.03])
+elevation_slider = plt.Slider(elevation_slider_ax, 'Elevation', -90, 90, valinit=30)
+azimuth_slider = plt.Slider(azimuth_slider_ax, 'Azimuth', -180, 180, valinit=-30)
+
+# Update view when sliders are changed
+elevation_slider.on_changed(update_view)
+azimuth_slider.on_changed(update_view)
+
+#Create the animation using FuncAnimation
+frames_per_second = 10  # For example, 10 frames per second
+duration_of_animation = t[-1]  # Duration of the animation in seconds
+frames = int(frames_per_second * duration_of_animation)
+frames = min(len(px1), len(px2), len(px3))  # Number of frames (assumes equal lengths for all joints)
+animation = FuncAnimation(fig, update, frames=frames, interval = 1000/frames_per_second, blit=True)
+
+# Display the animation
+plt.show()
---
+++ Serial.py
@@ -0,0 +1,177 @@
+import serial
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# Replace 'COM3' with the appropriate port name for your system (e.g., '/dev/ttyUSB0' on Linux or 'COM3' on Windows)
+ser = serial.Serial('COM4', 9600, timeout=1)  # Open serial port with baud rate 9600
+# data_saved_string = np.array([])
+# data_saved = np.array([])
+data_saved = np.array([])
+data_saved_string = []
+try:
+    if ser.is_open:
+        print("Serial port is open.")
+
+        while True:
+            # Read data from serial port
+            data = ser.readline().decode().strip()
+            if data:
+                print("Received:", data)
+                data_saved_string += [data.split(",")]
+                #data_saved += [data_saved_string.astype(float)]
+                
+                # # data_saved_string = np.append(data_saved,data.split(","))
+                # data_saved = np.append(data_saved,data_saved_string.astype(float))
+                # plt.plot(data_saved[0],data_saved[1])
+            # Add a small delay to not overload the serial port
+            #time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("Keyboard Interrupt. Closing serial port.")
+    ser.close()  # Close the serial port
+except serial.SerialException as e:
+    print("Serial Exception:", e)
+    ser.close()  # Close the serial port if an exception occurs
+
+def convert_strings_to_floats(input_array):
+    output_array = np.array([])
+    array_list = []
+    for i in range(0,len(input_array)):
+        row = np.array([])
+        for element in input_array[i]:
+            converted_float = float(element)
+            row = np.append(row,[converted_float])
+            
+        array_list += [row]
+        output_array = np.vstack(array_list)
+    return output_array
+
+output_array = convert_strings_to_floats(data_saved_string)
+output_array = output_array[1:]
+## Accelerations
+t = output_array[:,0]       #time
+ax1 = output_array[:,1]     # IMU 1
+ay1 = output_array[:,2]
+az1 = output_array[:,3]     # IMU 2
+ax2 = output_array[:,4]
+ay2 = output_array[:,5]
+az2 = output_array[:,6]
+ax3 = output_array[:,7]     # IMU 3
+ay3 = output_array[:,8]
+az3 = output_array[:,9]
+
+def euler(accelerations, timestamps):
+    velocities = [0.0]  # Initial velocity at timestamp 0
+    positions = [0.0]    # Initial position at timestamp 0
+    time_diff = np.diff(timestamps)  # Calculate time differences
+
+    print(time_diff)
+    for i in range(1,len(accelerations)):
+        # Assuming constant acceleration between irregular timestamps
+        delta_t = time_diff[i] if i < len(time_diff) else time_diff[-1]  # Last time diff for extrapolation
+
+        # Calculate velocity using Euler's method
+        #new_velocity = velocities[-1] + (accelerations[i]/accelerations[0]) * delta_t
+        new_velocity = (accelerations[i] - accelerations[i-1]) * delta_t * 9.81
+        print("accelerations:", accelerations[i], "time", delta_t, "old speed", velocities[-1])
+        velocities.append(new_velocity)
+
+        # Calculate position using velocity and Euler's method
+
+
+        new_position = positions[-1] + velocities[-1] * delta_t
+        positions.append(new_position)
+
+    # Convert velocity and position lists to NumPy arrays
+    velocities_array = np.array(velocities)
+    positions_array = np.array(positions)
+    velocities_array = velocities_array[:len(velocities_array)]
+    positions_array = positions_array[:len(positions_array)]
+    print(velocities_array)
+    print(positions_array)
+
+    return velocities_array, positions_array
+
+## Velocities
+vx1, px1 = euler(ax1,t)     # IMU 1
+vy1, py1 = euler(ay1,t)
+vz1, pz1 = euler(az1,t)
+vx2, px2 = euler(ax2,t)
+vy2, py2 = euler(ay2,t)
+vz2, pz2 = euler(az2,t)     
+vx3, px3 = euler(ax3,t)
+vy3, py3 = euler(ay3,t)
+vz3, pz3 = euler(az3,t)
+
+#from initial position
+initial_pos1 = np.array([0,0.5,0.2])
+initial_pos2 = np.array([0,0.5,0.55])
+initial_pos3 = np.array([0,0,1.1])
+px1 = initial_pos1[0] - px1
+py1 = initial_pos1[1] - py1
+pz1 = initial_pos1[2] - pz1
+px2 = initial_pos2[0] - px2
+py2 = initial_pos2[1] - py2
+pz2 = initial_pos2[2] - pz2
+px3 = initial_pos3[0] - px3
+py3 = initial_pos3[1] - py3
+pz3 = initial_pos3[2] - pz3
+
+print(pz1)
+print(pz2)
+print(pz3)
+
+# ## Positions
+# px1 = euler(vx1,t)     # IMU 1
+# py1 = euler(vy1,t)
+# pz1 = euler(vz1,t)
+# px2 = euler(vx2,t)
+# py2 = euler(vy2,t)
+# pz2 = euler(vz2,t)     
+# px3 = euler(vx3,t)
+# py3 = euler(vy3,t)
+# pz3 = euler(vz3,t)
+
+## Plot Accelerations
+imu_acc = [ax1,ay1,az1,ax2,ay2,az2,ax3,ay3,az3]
+imu_acc_label = ['ax1','ay1','az1','ax2','ay2','az2','ax3','ay3','az3']
+
+for i in range(0,len(imu_acc)):
+    plt.plot(t,imu_acc[i], label = imu_acc_label[i], linewidth = 1)
+plt.xlabel('Time (s)')
+plt.ylabel('Acceleration (m/s^2)')
+plt.title('IMU Accelerations')
+plt.legend(loc = "upper right")
+plt.grid(True)
+plt.show()
+
+## Plot Velocities
+imu_vel = [vx1,vy1,vz1,vx2,vy2,vz2,vx3,vy3,vz3]
+imu_vel_label = ['vx1','vy1','vz1','vx2','vy2','vz2','vx3','vy3','vz3']
+
+# plot of all IMU readings
+for i in range(0,len(imu_vel)):
+    plt.plot(t,imu_vel[i], label = imu_vel_label[i], linewidth = 1)
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (m/s)')
+plt.title('IMU Velocities')
+plt.legend(loc = "upper right")
+plt.grid(True)
+plt.show()
+
+## Plot Positions
+imu_pos = [px1,py1,pz1,px2,py2,pz2,px3,py3,pz3]
+imu_pos_label = ['px1','py1','pz1','px2','py2','pz2','px3','py3','pz3']
+
+# plot of all IMU readings
+for i in range(0,len(imu_pos)):
+    plt.plot(t,imu_pos[i], label = imu_pos_label[i], linewidth = 1)
+plt.xlabel('Time (s)')
+plt.ylabel('Position m')
+plt.title('IMU Positions')
+plt.legend(loc = "upper right")
+plt.grid(True)
+plt.show()
